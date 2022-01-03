596af718f7e36a5447e6cdfc1ae5d21bc8d2f857
kuatroka <aaarmani_2000@yahoo.com>
2022-01-03 22:50:00 +0000

    get and post requests

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+    
 
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
+           {"title": "favourite food", "content": "I like pizza", "id": 2}]
 
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello Sergio"}
 
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "this is your post"}
+    return {"data": my_posts}
 
-@app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"message": "succesfully created posts"}
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+    # return {"new_post": f"title: {payload['title']} content: {payload['content']} "}
+
+
+# title str, content str
 
 
 @app.get("/plonker")
