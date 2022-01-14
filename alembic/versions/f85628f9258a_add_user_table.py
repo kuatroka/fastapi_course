2a18964edf69089b460def0fd6c2a1f89acb3c99
kuatroka <aaarmani_2000@yahoo.com>
2022-01-14 20:41:45 +0000

    adding now() to users table

--- alembic/versions/f85628f9258a_add_user_table.py
+++ alembic/versions/f85628f9258a_add_user_table.py
@@ -16,12 +16,15 @@ depends_on = None
 
 
 def upgrade():
-    op.create_table("users", sa.Column("id", sa.Integer(), nullable=False),
-                    sa.Column("email", sa.String(), nullable=False),
-                    sa.Column("password", sa.String(), nullable=False),
-                    sa.Column("created_at", sa.TIMESTAMP(timezone=True)),
-                    sa.PrimaryKeyConstraint("id"),
-                    sa.UniqueConstraint("email"))
+    op.create_table(
+        "users", sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("email", sa.String(), nullable=False),
+        sa.Column("password", sa.String(), nullable=False),
+        sa.Column("created_at",
+                  sa.TIMESTAMP(timezone=True),
+                  nullable=False,
+                  server_default=sa.text('now()')),
+        sa.PrimaryKeyConstraint("id"), sa.UniqueConstraint("email"))
     pass
 
 
